8096b92cb34488a161698f00f86adeac94753420
york <377262688@qq.com>
2020-08-02 15:01:50 +0800

    增加监控

--- user/src/main/java/com/york/user/remote/OrderService.java
+++ user/src/main/java/com/york/user/remote/OrderService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author york
  * @create 2020-07-29 15:41
  **/
-@FeignClient(name = "order",fallback = OrderServiceHystrix.class)
+@FeignClient(name = "order")
 public interface OrderService {
 
     @GetMapping("order/getOrder")
